46ed1119f474ddcca228dfdca531a78d356d3051
Hian <hian.hsm@gmail.com>
2023-03-29 15:49:41 -0300

    feat: token validation

--- src/jwtFunctions.ts
+++ src/jwtFunctions.ts
@@ -1,11 +1,13 @@
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
 import { TUser } from './types';
+
+const secret: string | undefined = process.env.JWT_SECRET;
 
 const generateToken = (user: TUser) => {
   const payload = { username: user.username };
   return jwt.sign(
     payload, 
-    process.env.JWT_SECRET as string,
+    secret as string,
     { algorithm: 'HS256', expiresIn: '1d' },
   );
 };
@@ -14,12 +16,15 @@ const loginToken = (user: TUser) => {
   const payload = { id: user.id, username: user.username };
   return jwt.sign(
     payload,
-    process.env.JWT_SECRET as string,
+    secret as string,
     { algorithm: 'HS256', expiresIn: '1d' },
   );
 };
 
+const tokenValidation = (token: string): string | JwtPayload => jwt.verify(token, secret as Secret);
+
 export default {
   generateToken,
   loginToken,
+  tokenValidation,
 };
---
+++ src/middlewares/jwtValidation.ts
@@ -0,0 +1,16 @@
+import { Response, Request, NextFunction } from 'express';
+import jwt from '../jwtFunctions';
+
+export default async function jwtValidation(req: Request, res: Response, next: NextFunction) {
+  const { authorization } = req.headers;
+
+  if (!authorization) res.status(401).json({ message: 'Token not found' });
+
+  try {
+    const payload = jwt.tokenValidation(authorization as string);
+    req.body.user = payload;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+}
