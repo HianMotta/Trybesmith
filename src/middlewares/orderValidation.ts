9ba57b2810322f640f0efee6627a75f73b8cfbd2
Hian <hian.hsm@gmail.com>
2023-03-29 16:04:29 -0300

    feat: order validation

---
+++ src/middlewares/orderValidation.ts
@@ -0,0 +1,23 @@
+import { Response, Request, NextFunction } from 'express';
+import Joi from 'joi';
+
+const orderSchema = Joi.array().items(Joi.number().integer()).min(1).required()
+  .messages({
+    'any.required': '"productsIds" is required',
+    'array.base': '"productsIds" must be an array',
+    'array.min': '"productsIds" must include only numbers',
+    'number.base': '"productsIds" must include only numbers',
+  });
+
+export default async function orderValidation(req: Request, res: Response, next: NextFunction) {
+  const { body } = req;
+
+  const { error } = orderSchema.validate(body);
+
+  if (error) {
+    const httpStatus = error.message.includes('required') ? 400 : 422;
+    return res.status(httpStatus).json({ message: error.message });
+  }
+
+  next();
+}
